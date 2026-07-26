08c5a6385977d31c98072bc7ddc83a685df9b135
AstelCode <frankparedesalpi@gmail.com>
2026-07-25 23:11:08 -0500

    fix token services

--- src/infrastructure/services/auth/FastifyJwtService.ts
+++ src/infrastructure/services/auth/FastifyJwtService.ts
@@ -5,13 +5,18 @@ export class FastifyJwtService implements TokenService {
   constructor(private readonly app: FastifyInstance) {}
 
   async generate(payload: { id: bigint; email: string }): Promise<string> {
-    return this.app.jwt.sign(payload);
+    return this.app.jwt.sign({
+      ...payload,
+      id: payload.id.toString(),
+    });
   }
 
   async verify(token: string): Promise<{
     id: bigint;
     email: string;
   }> {
-    return this.app.jwt.verify(token);
+    const data = (await this.app.jwt.verify(token)) as any;
+    data.id = BigInt(data.id);
+    return data as any;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,42 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
 import jwt from "@fastify/jwt";
+import { PostgresUserRepository } from "./infrastructure/database/postgres/PostgresUserRepository";
+import { ArgonHashService } from "./infrastructure/services/hash/ArgonHashService";
+import { FastifyJwtService } from "./infrastructure/services/auth/FastifyJwtService";
+import { CreateUserUseCase } from "./application/usecases/user/CreateUserUseCase";
+import { LoginUseCase } from "./application/usecases/user/LoginUseCase";
 dotenv.config();
 
-// const app = Fastify({
-//   logger: true,
-// });
+const app = Fastify({
+  logger: true,
+});
 
-// app.register(jwt, {
-//   secret: process.env.JWT_SECRET!,
-// });
+app.register(jwt, {
+  secret: process.env.JWT_SECRET!,
+});
+async function main() {
+  const app = Fastify({ logger: true });
 
-// // app.get("/", async () => {
-// //   return {
-// //     mesage: "Hola Fastify",
-// //   };
-// // });
+  await app.register(jwt, {
+    secret: process.env.JWT_SECRET!,
+  });
 
-// // app.listen({
-// //   port: 3000,
-// // });
-// const repository = new PrimsaModRespository();
+  await app.ready();
 
-// try {
-//   repository.listAll().then((mods) => {
-//     console.log("Mods encontrados:", mods);
-//   });
-// } catch (error) {
-//   console.error("Error al consultar la base de datos:", error);
-// }
+  const repository = new PostgresUserRepository();
+  const hashService = new ArgonHashService();
+  const tokenService = new FastifyJwtService(app);
+
+  const loginUseCase = new LoginUseCase(repository, tokenService, hashService);
+
+  //const token = await createUser.create("frankparedesalpi@gmail.com", "123456");
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMiLCJlbWFpbCI6ImZyYW5rcGFyZWRlc2FscGlAZ21haWwuY29tIiwiaWF0IjoxNzg1MDM4ODMwfQ.nV1FtoS-Lj1F1aZjpakdQ8_OZHGa63R0lvYBmt8fzmI";
+
+  console.log(await tokenService.verify(token));
+  //await  loginUseCase.login("")
+}
+
+main().catch(console.error);
